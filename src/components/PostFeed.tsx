4c9fa96cf8389c4c576db0a48de07b2cbdacb3ce
kenneth vega <kenneth.trinidad.vega@gmail.com>
2022-10-24 10:13:11 +0800

    added a search functionality

--- src/components/PostFeed.tsx
+++ src/components/PostFeed.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import styles from "./PostFeed.module.scss";
 import PostItem from "./PostItem";
 import { DocumentData } from "firebase/firestore";
@@ -5,10 +6,36 @@ import { Document } from "../ts/types/document";
 import PostItemSkeleton from "./skeletons/PostItemSkeleton";
 
 const PostFeed = ({ documents }: DocumentData) => {
+  const [filteredDocs, setFilteredDocs] = useState([]);
+  const [filters, setFilters] = useState({ s: "" });
+  // search function
+  useEffect(() => {
+    if (documents) {
+      let docs = documents.filter(
+        (doc: any) =>
+          doc.title.toLowerCase().indexOf(filters.s.toLowerCase()) >= 0
+      );
+      setFilteredDocs(docs);
+    }
+  }, [filters]);
+  //
+
+  const search = (s: any) => {
+    setFilters({
+      s,
+    });
+  };
+
   return (
     <div className={styles["posts-container"]}>
+      <input
+        type="text"
+        className={styles.search}
+        placeholder="Search a doc"
+        onKeyUp={(e) => search((e.target as HTMLInputElement).value)}
+      />
       {documents &&
-        documents.map((document: Document) => {
+        filteredDocs.map((document: Document) => {
           return <PostItem document={document} key={document.slug} />;
         })}
       {!documents && (
